e466cbea5d745f9409ab6e5d6b3c35f86c74a89c
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-03-10 21:05:06 -0300

    feat: create file of configuration of next-auth

---
+++ src/auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export const { handlers: { GET, POST }, auth } = NextAuth({
+  providers: [ GoogleProvider({
+    clientId: process.env.GOOGLE_ID ?? "",
+    clientSecret: process.env.GOOGLE_SECRET ?? ""
+  })],
+})
